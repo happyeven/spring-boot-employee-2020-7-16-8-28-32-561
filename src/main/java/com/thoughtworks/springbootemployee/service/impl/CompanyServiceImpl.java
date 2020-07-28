6c269bf4d7c25f4c3a54aadf9c8048ce0d26cc41
Domingo <986224568@qq.com>
2020-07-28 18:51:52 +0800

    [spring-boot-employee][Domingo] feature add CompanyService, CompanyServiceImpl

---
+++ src/main/java/com/thoughtworks/springbootemployee/service/impl/CompanyServiceImpl.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.springbootemployee.service.impl;
+
+import com.thoughtworks.springbootemployee.entity.Employee;
+import com.thoughtworks.springbootemployee.service.CompanyService;
+
+import java.util.List;
+
+public class CompanyServiceImpl implements CompanyService {
+    @Override
+    public List<Employee> getALLEmployee() {
+        return null;
+    }
+}
