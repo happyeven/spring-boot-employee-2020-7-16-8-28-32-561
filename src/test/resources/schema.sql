d45fc1e0d2abc606eca7729dcdef2a1cf16f9fab
howevert <1413537501@qq.com>
2020-07-31 11:05:30 +0800

    [spring-boot-employee][david and dong] feature: install flyway and create two table by it

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -7,11 +7,7 @@ CREATE TABLE employee (
   gender VARCHAR(10) DEFAULT NULL,
   company_id INT Not NULL
 );
-INSERT INTO employee (name, age, gender,company_id) VALUES
- ('lmh','18','male',1),
- ('htw','18','male',1);
  CREATE TABLE company (
    company_id INT AUTO_INCREMENT  PRIMARY KEY,
    name VARCHAR(100) NOT NULL
  );
- INSERT INTO company (name) VALUES('oocl');
