f7e2b2b7978e9421647a88458d53bbfce5845fb2
howevert <1413537501@qq.com>
2020-07-31 12:48:08 +0800

    [spring-boot-employee][david and dong] feature : pass should_return_ok_when_finAll_company

---
+++ src/test/java/com/thoughtworks/springbootemployee/CompangIntergratorTest.java
@@ -0,0 +1,40 @@
+package com.thoughtworks.springbootemployee;
+
+import com.thoughtworks.springbootemployee.Repository.CompanyRepository;
+import com.thoughtworks.springbootemployee.entity.Company;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CompangIntergratorTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @AfterEach
+    void tearDown() {
+        companyRepository.deleteAll();
+    }
+
+    @Test
+    void should_return_ok_when_finAll_company() throws Exception {
+        mockMvc.perform(get("/companies")).andExpect(status().isOk());
+    }
+
+
+}
