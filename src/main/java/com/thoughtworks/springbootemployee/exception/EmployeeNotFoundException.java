2713c3df3a3b5eb7bfe46b57aa29eb6f664860b0
HoweverT <1413537501@qq.com>
2020-08-01 20:44:23 +0800

    Revert "[spring-boot-employee][david and dong] refactor : refactor the message of exception handle"
    This reverts commit b1d194be

--- src/main/java/com/thoughtworks/springbootemployee/exception/EmployeeNotFoundException.java
+++ src/main/java/com/thoughtworks/springbootemployee/exception/EmployeeNotFoundException.java
@@ -5,8 +5,4 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class EmployeeNotFoundException extends RuntimeException {
-    @Override
-    public String getMessage() {
-        return "can't find the Employee.";
-    }
 }
