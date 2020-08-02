3a37348a43d520cb95c64cacd1b44959004d9c6b
happyeven <1361840595@qq.com>
2020-08-02 18:41:32 +0800

    [spring-boot-employee][david and dong] test :should_return_dto_with_company_when_entity_to_dto_given_employee_with_exist_company

--- src/test/java/com/thoughtworks/springbootemployee/MapperTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/MapperTest.java
@@ -16,15 +16,16 @@ public class MapperTest {
     @Test
     void should_return_age_1_when_map_dto_to_entity_given_dto_with_age_1() {
         //given
-        EmployeeRequestDTO employeeRequestDTO =new EmployeeRequestDTO();
+        EmployeeRequestDTO employeeRequestDTO = new EmployeeRequestDTO();
         employeeRequestDTO.setAge(1);
         Company company = new Company("oocl");
 
         //when
-        Employee employee = EmployeeMapper.employeeRequestDtoToEmployee(employeeRequestDTO,company);
+        Employee employee = EmployeeMapper.employeeRequestDtoToEmployee(employeeRequestDTO, company);
         //then
-        assertEquals(1,employee.getAge());
+        assertEquals(1, employee.getAge());
     }
+
     @Test
     void should_return_david_when_map_dto_to_entity_when_given_dto_with_name_david() {
         //given
@@ -32,20 +33,30 @@ public class MapperTest {
         employeeRequestDTO.setName("david");
         Company company = new Company("oocl");
         //when
-        Employee employee = EmployeeMapper.employeeRequestDtoToEmployee(employeeRequestDTO,company);
+        Employee employee = EmployeeMapper.employeeRequestDtoToEmployee(employeeRequestDTO, company);
         //then
-        assertEquals("david",employee.getName());
+        assertEquals("david", employee.getName());
     }
 
     @Test
     void should_return_male_when_map_dto_to_entity_when_given_dto_with_gender_male() {
         //given
-        EmployeeRequestDTO employeeRequestDTO = new EmployeeRequestDTO(1,"david","male");
+        EmployeeRequestDTO employeeRequestDTO = new EmployeeRequestDTO(1, "david", "male");
         Company company = new Company("oocl");
         //when
-        Employee employee = new Employee(1,"david","male",company);
-        Employee entity = EmployeeMapper.employeeRequestDtoToEmployee(employeeRequestDTO,company);
-        assertEquals(employee,entity);
+        Employee employee = new Employee(1, "david", "male", company);
+        Employee entity = EmployeeMapper.employeeRequestDtoToEmployee(employeeRequestDTO, company);
+        assertEquals(employee, entity);
     }
 
+    @Test
+    void should_return_dto_with_company_when_entity_to_dto_given_employee_with_exist_company() {
+        Company company = new Company("oocl");
+        Employee employee = new Employee(19, "dong", "male", company);
+        EmployeeResponseDTO employeeResponseDTO = EmployeeMapper.employeeToEmployeeResponse(employee);
+        Assertions.assertEquals(employee.getAge(), employeeResponseDTO.getAge());
+        Assertions.assertEquals(employee.getCompany().getName(), employeeResponseDTO.getCompanyName());
+        Assertions.assertEquals(employee.getGender(), employeeResponseDTO.getGender());
+        Assertions.assertEquals(employee.getName(), employeeResponseDTO.getName());
+    }
 }
