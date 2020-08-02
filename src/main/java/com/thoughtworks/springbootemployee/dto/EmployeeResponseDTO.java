8c5d59f25578845df904ab7615f7014e180412b3
happyeven <1361840595@qq.com>
2020-08-02 15:41:10 +0800

    [spring-boot-employee][david and dong] refactor: add gender attribute in EmployeeResponseDTO and add an constructor

--- src/main/java/com/thoughtworks/springbootemployee/dto/EmployeeResponseDTO.java
+++ src/main/java/com/thoughtworks/springbootemployee/dto/EmployeeResponseDTO.java
@@ -4,6 +4,23 @@ public class EmployeeResponseDTO {
     private String name;
     private Integer age;
     private String companyName;
+    private String gender;
+
+    public EmployeeResponseDTO(String name, Integer age, String companyName, String gender) {
+        this.name = name;
+        this.age = age;
+        this.companyName = companyName;
+        this.gender = gender;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
     public String getName() {
         return name;
     }
