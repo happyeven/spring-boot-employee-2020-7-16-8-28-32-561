135955c807e80961803ad23a49c50b34ef005b0e
happyeven <1361840595@qq.com>
2020-08-02 17:44:59 +0800

    [spring-boot-employee][david and dong] refactor : delete the useless methods

--- src/main/java/com/thoughtworks/springbootemployee/dto/EmployeeResponseDTO.java
+++ src/main/java/com/thoughtworks/springbootemployee/dto/EmployeeResponseDTO.java
@@ -25,37 +25,9 @@ public class EmployeeResponseDTO {
         return name;
     }
 
-    public String getCompanyName() {
-        return companyName;
-    }
-
-    public void setCompanyName(String companyName) {
-        this.companyName = companyName;
-    }
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public EmployeeResponseDTO(String name, int age, String companyName) {
-        this.name = name;
-        this.age = age;
-        this.companyName = companyName;
-    }
-
-    public EmployeeResponseDTO(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    public EmployeeResponseDTO() {
-    }
 }
