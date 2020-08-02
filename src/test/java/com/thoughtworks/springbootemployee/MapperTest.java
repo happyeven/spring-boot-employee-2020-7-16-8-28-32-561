ec2b9554ab8d29a19ae348fd0947958c300dde48
happyeven <1361840595@qq.com>
2020-08-02 18:41:10 +0800

    [spring-boot-employee][david and dong] test :should_return_male_when_map_dto_to_entity_when_given_dto_with_gender_male

--- src/test/java/com/thoughtworks/springbootemployee/MapperTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/MapperTest.java
@@ -36,4 +36,16 @@ public class MapperTest {
         //then
         assertEquals("david",employee.getName());
     }
+
+    @Test
+    void should_return_male_when_map_dto_to_entity_when_given_dto_with_gender_male() {
+        //given
+        EmployeeRequestDTO employeeRequestDTO = new EmployeeRequestDTO(1,"david","male");
+        Company company = new Company("oocl");
+        //when
+        Employee employee = new Employee(1,"david","male",company);
+        Employee entity = EmployeeMapper.employeeRequestDtoToEmployee(employeeRequestDTO,company);
+        assertEquals(employee,entity);
+    }
+
 }
