b7ca2450e2378fb760008068a1fb92bb74db2ba6
happyeven <1361840595@qq.com>
2020-08-02 18:39:31 +0800

    [spring-boot-employee][david and dong] refactor :delete some test methods

--- src/test/java/com/thoughtworks/springbootemployee/SpringBootEmployeeApplicationTests.java
+++ src/test/java/com/thoughtworks/springbootemployee/SpringBootEmployeeApplicationTests.java
@@ -128,41 +128,4 @@ class SpringBootEmployeeApplicationTests {
 		assertEquals(2, result.size());
 	}
 
-	@Test
-	void should_return_age_1_when_map_dto_to_entity_given_dto_with_age_1() {
-		//given
-		EmployeeRequestDTO employeeRequestDTO =new EmployeeRequestDTO();
-		employeeRequestDTO.setAge(1);
-		Company company = new Company("oocl");
-
-		//when
-		Employee employee = EmployeeMapper.employeeRequestDtoToEmployee(employeeRequestDTO,company);
-		//then
-		assertEquals(1,employee.getAge());
-	}
-
-	@Test
-	void should_return_david_when_map_dto_to_entity_when_given_dto_with_name_david() {
-		//given
-		EmployeeRequestDTO employeeRequestDTO = new EmployeeRequestDTO();
-		employeeRequestDTO.setName("david");
-		Company company = new Company("oocl");
-		//when
-		Employee employee = EmployeeMapper.employeeRequestDtoToEmployee(employeeRequestDTO,company);
-		//then
-		assertEquals("david",employee.getName());
-	}
-
-	@Test
-	void should_return_male_when_map_dto_to_entity_when_given_dto_with_gender_male() {
-		//given
-		EmployeeRequestDTO employeeRequestDTO = new EmployeeRequestDTO(1,"david","male");
-		Company company = new Company("oocl");
-		//when
-		Employee employee = new Employee(1,"david","male",company);
-		Employee entity = EmployeeMapper.employeeRequestDtoToEmployee(employeeRequestDTO,company);
-
-		assertEquals(employee,entity);
-
-	}
 }
